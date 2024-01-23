0703510eb9efab99d170faee10c9e363e8d7891f
simuseokyun <sim31059999@gmail.com>
2024-01-23 18:37:34 +0900

    feat : 코드 처리하기

--- kakao-gift/main.js
+++
@@ -1,8 +0,0 @@
-const solution = () => {
-    // if (friends.length < 2 || friends.length > 50) {
-    //     return;
-    // }
-    const friends = ['a', 'b', 'c'];
-    const gift = ['a b', 'b a', 'c a', 'a c', 'a c', 'c a'];
-};
-solution();
